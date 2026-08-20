2839206772f431ab00e921253245d74b41111c23
Het Kapadia <121884585+HetK2004@users.noreply.github.com>
2026-08-20 10:54:45 +0100

    Time: 3302 ms (5.92%), Space: 18.9 MB (100%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        l = []
+        for i in nums:
+            if nums.count(i) == 1:
+                l.append(i)
+            else:
+                pass
+        return l
